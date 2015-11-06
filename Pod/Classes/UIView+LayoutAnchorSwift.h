c791fa384e17e32999f844d00960e913f3b9c2ba
Alex Pretzlav <alex@turnlav.net>
2015-11-06 15:33:35 -0600

    Fix typos in LayoutAnchorSwift.h comment

--- Pod/Classes/UIView+LayoutAnchorSwift.h
+++ Pod/Classes/UIView+LayoutAnchorSwift.h
@@ -8,10 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-//These helpers have been added to make swift comiplation works
-//In swift apis that are available for an iOS version and up, wont compile.
-//To fix this you can guard agains the api. However, for a more transparent usage for anchors on ios
-//7,8 and 9, if you are using swift, use oa_ prefixed version.
+// These helpers have been added to make Swift compilation work
+// In Swift, APIs that are available for an iOS version and up wont compile on older SDK versions.
+// To fix this you can guard against the API. However, for a more transparent usage for anchors on iOS
+// 7, 8, and 9, if you are using Swift, use oa_ prefixed version.
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < 90000
 #import <OALayoutAnchor/UIView+OALayoutAnchor.h>
